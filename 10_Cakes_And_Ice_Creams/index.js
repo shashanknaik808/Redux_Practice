d2424b1e1d0d46edfda07cbc31f875e995443521
Shashank Naik <shashanknaik808@gmail.com>
2024-04-16 21:49:28 +0530

    Implemented cake restocking functionality in Redux with corresponding action and reducer

---
+++ 10_Cakes_And_Ice_Creams/index.js
@@ -0,0 +1,63 @@
+const redux = require('redux')
+const createStore = redux.createStore
+const bindActionCreators = redux.bindActionCreators
+
+const CAKE_ORDERED = 'CAKE_ORDERED'
+const CAKE_RESTOCKED = 'CAKE_RESTOCKED'
+
+function orderCake() {
+    return {
+        type: CAKE_ORDERED,
+        payload: 1,
+    }
+}
+
+function restockCake(qty = 1) {
+    return {
+        type: CAKE_RESTOCKED,
+        payload: qty,
+    }
+}
+
+const initialState = {
+    numOfCakes: 10,
+}
+
+function reducer(state = initialState, action) {
+    switch (action.type) {
+        case CAKE_ORDERED:
+            return {
+                ...state,
+                numOfCakes: state.numOfCakes - 1,
+            }
+
+        case CAKE_RESTOCKED:
+            return {
+                ...state,
+                numOfCakes: state.numOfCakes + action.payload,
+            }
+
+        default:
+            return state
+    }
+}
+
+const store = createStore(reducer)
+console.log('Initial state', store.getState());
+
+const unsubscribe = store.subscribe(function () {
+    console.log('Update state', store.getState());
+});
+
+// store.dispatch(orderCake())
+// store.dispatch(orderCake())
+// store.dispatch(orderCake())
+// store.dispatch(restockCake(3))
+
+const actions = bindActionCreators({ orderCake, restockCake }, store.dispatch)
+actions.orderCake()
+actions.orderCake()
+actions.orderCake()
+actions.restockCake(3)
+
+unsubscribe()
