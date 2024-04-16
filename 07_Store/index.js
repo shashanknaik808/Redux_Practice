bcddb2d60a1fd97f4d5cd0f337e9695ac743072c
Shashank Naik <shashanknaik808@gmail.com>
2024-04-16 19:19:36 +0530

    Implemented store and dispatch actions for cake ordering

--- 07_Store/index.js
+++ 07_Store/index.js
@@ -1,3 +1,6 @@
+const redux = require('redux')
+const createStore = redux.createStore
+
 const CAKE_ORDERED = 'CAKE_ORDERED'
 
 function orderCake() {
@@ -23,3 +26,16 @@ function reducer(state = initialState, action) {
             return state
     }
 }
+
+const store = createStore(reducer)
+console.log('Initial state', store.getState());
+
+const unsubscribe = store.subscribe(function () {
+    console.log('Update state', store.getState());
+});
+
+store.dispatch(orderCake())
+store.dispatch(orderCake())
+store.dispatch(orderCake())
+
+unsubscribe()
