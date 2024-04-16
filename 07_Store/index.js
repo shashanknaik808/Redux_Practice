1d7618bcef29696c95113993dc0e2641165e0ab6
Shashank Naik <shashanknaik808@gmail.com>
2024-04-16 18:47:13 +0530

    Implemented reducer function for cake ordering

--- 07_Store/index.js
+++ 07_Store/index.js
@@ -1,1 +1,25 @@
-console.log('From index.js');
+const CAKE_ORDERED = 'CAKE_ORDERED'
+
+function orderCake() {
+    return {
+        type: CAKE_ORDERED,
+        quantity: 1,
+    }
+}
+
+const initialState = {
+    numOfCakes: 10,
+}
+
+function reducer(state = initialState, action) {
+    switch (action.type) {
+        case CAKE_ORDERED:
+            return {
+                ...state,
+                numOfCakes: state.numOfCakes - 1,
+            }
+
+        default:
+            return state
+    }
+}
