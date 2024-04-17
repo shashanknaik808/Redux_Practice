ffe1ac5fa07643bdbbdcaf75223aa50bae2d0b53
Shashank Naik <shashanknaik808@gmail.com>
2024-04-17 16:37:10 +0530

    Using Immer

---
+++ 14_Middleware/nested-state.js
@@ -0,0 +1,49 @@
+const redux = require('redux')
+const produce = require('immer').produce
+
+const initialState = {
+    name: 'Vishwas',
+    address: {
+        street: '123 Main St',
+        city: 'Boston',
+        State: 'MA',
+    }
+}
+
+const STREET_UPDATED = 'STREET_UPDATED'
+function updateStreet(street) {
+    return {
+        type: STREET_UPDATED,
+        payload: street,
+    }
+}
+
+
+function reducer(state = initialState, action) {
+    switch (action.type) {
+        case STREET_UPDATED:
+            // return {
+            //     ...state,
+            //     address: {
+            //         ...state.address,
+            //         street: action.payload,
+            //     },
+            // }
+            return produce(state, (draft) => {
+                draft.address.street = action.payload
+            })
+        default: {
+            return state
+        }
+    }
+}
+
+const store = redux.createStore(reducer)
+console.log('Initial State', store.getState());
+
+const unsubscribe = store.subscribe(() => {
+    console.log('Updated State', store.getState());
+})
+
+store.dispatch(updateStreet('456 Main St'))
+unsubscribe()
