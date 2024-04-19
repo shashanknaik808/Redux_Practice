8ed5e6014e1e6cd2677eae805823f6e7a5f75b9e
Shashank Naik <shashanknaik808@gmail.com>
2024-04-19 12:13:58 +0530

    Creating and exporting Redux store

---
+++ 21_Ice_Cream_Feature/app/store.js
@@ -0,0 +1,11 @@
+const configureStore = require('@reduxjs/toolkit').configureStore
+const cakeReducer = require('../features/cake/cakeSlice.js')
+
+
+const store = configureStore({
+    reducer: {
+        cake: cakeReducer,
+    }
+})
+
+module.exports = store
