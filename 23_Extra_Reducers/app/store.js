3990a9ae082319c99ef54c775dd6f078752ac596
Shashank Naik <shashanknaik808@gmail.com>
2024-04-19 13:19:12 +0530

    Adding icecream features

--- 23_Extra_Reducers/app/store.js
+++ 23_Extra_Reducers/app/store.js
@@ -1,10 +1,11 @@
 const configureStore = require('@reduxjs/toolkit').configureStore
 const cakeReducer = require('../features/cake/cakeSlice.js')
-
+const icecreamReducer = require('../features/icecream/icecreamSlice.js')
 
 const store = configureStore({
     reducer: {
         cake: cakeReducer,
+        icecream: icecreamReducer,
     }
 })
 
--- 23_Extra_Reducers/index.js
+++ 23_Extra_Reducers/index.js
@@ -1,6 +1,6 @@
 const store = require('./app/store.js');
 const cakeActions = require('./features/cake/cakeSlice.js').cakeActions;
-
+const icecreamActions = require('./features/icecream/icecreamSlice.js').icecreamActions
 console.log('Initial state', store.getState());
 const unsubscribe = store.subscribe(function () {
     console.log('Updated state', store.getState());
@@ -10,5 +10,9 @@ store.dispatch(cakeActions.ordered());
 store.dispatch(cakeActions.ordered());
 store.dispatch(cakeActions.ordered());
 store.dispatch(cakeActions.restocked(3));
+
+store.dispatch(icecreamActions.ordered());
+store.dispatch(icecreamActions.ordered());
+store.dispatch(icecreamActions.restocked(2));
 
 unsubscribe();
