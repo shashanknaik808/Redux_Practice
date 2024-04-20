99e1deab9e4cde9955f2c3f619ffad6a11025f94
Shashank Naik <shashanknaik808@gmail.com>
2024-04-20 15:08:24 +0530

    Using logger Middleware

--- 24_Async_Thunks/index.js
+++ 24_Async_Thunks/index.js
@@ -1,9 +1,9 @@
 const store = require('./app/store.js');
 const cakeActions = require('./features/cake/cakeSlice.js').cakeActions;
 const icecreamActions = require('./features/icecream/icecreamSlice.js').icecreamActions
+
 console.log('Initial state', store.getState());
 const unsubscribe = store.subscribe(function () {
-    console.log('Updated state', store.getState());
 });
 
 store.dispatch(cakeActions.ordered());
