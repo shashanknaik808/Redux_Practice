fb422bf6a9af36aafc1f311998b542c82df11646
Shashank Naik <shashanknaik808@gmail.com>
2024-04-21 22:05:18 +0530

    Created cakeView component with Order and Restock buttons

---
+++ 28_RPS_useDispatch/src/features/cake/cakeView.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+function cakeView() {
+    return (
+        <div>
+            <h2>Number of cakes - </h2>
+            <button>Order Cake</button>
+            <button>Restock cakes</button>
+
+        </div>
+    )
+}
+
+export default cakeView;
