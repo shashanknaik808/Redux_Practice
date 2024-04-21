5629c59a7fe8e850e901b4c0e4135aea07b34b86
Shashank Naik <shashanknaik808@gmail.com>
2024-04-21 12:46:20 +0530

    Created icecreamView component with Order and Restock buttons

--- 25_React_Project_Setup/src/features/icecream/icecreamView.jsx
+++ 25_React_Project_Setup/src/features/icecream/icecreamView.jsx
@@ -2,7 +2,11 @@ import React from 'react'
 
 function icecreamView() {
     return (
-        <div>icecreamView</div>
+        <div>
+            <h2>Number of ice creams - </h2>
+            <button>Order ice cream</button>
+            <button>Restock ice cream</button>
+        </div>
     )
 }
 
